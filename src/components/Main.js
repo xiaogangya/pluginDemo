928291c8b38d21cfcdad333f2f97e382367a38b8
yanz <yanz@microsoft.com>
2016-09-28 17:45:30 +0800

    Initialize

---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'yanz',
+      email: 'yanz@microsoft.com',
+      seat: 4571
+    };
+  }
+
+  render() {
+    return (
+      <div className="index">
+        <div>
+          App State:
+          <div><span>name: </span><span>{this.state.name}</span></div>
+          <div><span>email: </span><span>{this.state.email}</span></div>
+          <div><span>seat: </span><span>{this.state.seat}</span></div>
+        </div>
+        <div>
+          Plugin Area:
+        </div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
