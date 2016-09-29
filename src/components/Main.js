0f7ad345018fc494a21e5294a46dce911347b0d3
yanz <yanz@microsoft.com>
2016-09-29 11:51:32 +0800

    Clean up

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,4 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
-
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   constructor() {
